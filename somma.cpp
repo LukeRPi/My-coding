fb8389a7676893ead4c7f78f1c5d2d4a3ddc41f7
Luca <lukestarjair@gmail.com>
2021-08-28 17:32:13 +0200

    Added somma.cpp

---
+++ somma.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int a, b, c; // dichiara le variabili a,b e c
+
+int main() {
+    cout << "Inserisci un numero: "; // visualizza a schermo il testo tra virgolette
+    cin >> a; // rileva ciò che viene digitato a schermo
+
+    cout << "Inserisci un numero: ";
+    cin >> b;
+
+
+    c = a + b; // fai la somma dei numeri
+
+    cout << "La somma è " << c << endl; // visualizza a schermo il risultato e una riga vuota
+}
